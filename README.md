e2b80ee00cd0c0e33395b2b878283fecf1ebce45
oldmanz <h2oldmanz@gmail.com>
2024-02-22 18:09:07 -0500

    cache

--- README.md
+++ README.md
@@ -31,16 +31,16 @@ jobs = queue.jobs  ## List of jobs in queue object
 ### Running Jobs
 ```
 for job in queue.jobs:
-    job.run(function, arg, kwarg=kwarg)  ## run any function and pass any args to it
+    job.run(function)  ## run any function and pass any args to it
 ```
 function can be any function to run for this job
-any args and kwargs will be used in this function
-
-EX:
+any parameters set in laravel will be passed to this function by name:
 ```
-job.run(print, job.id)
+# laravel:
+$this->groupId = 12;
 
-Output: 12345
+# python
+function(groupId = 12)
 ```
 
 Jobs run in this way will be:
@@ -48,10 +48,41 @@ Jobs run in this way will be:
 - if failed, added to the failed jobs table along with the exception and a timestamp
 
 
+Additionally, you can specify a `cache_lock_uid`
+This can be either a str or a list of strings.
+This is to be used for managing the job cache if you are using the `ShouldBeUnique` or `ShouldBeUniqueUntilProcessing` properties in Laravel
+The `cache_lock_uid` should resemble what you have set `uniqueId()` to in laravel.
+If a list is sent, you can specify parameters to be filled with `$`:
+
+```
+# Laravel:
+$this->param1 = 1
+$this->param2 = 2
+
+public function uniqueId(): string
+    {
+        return $this->param1 . '-' . $this->param2;
+    }
+
+
+# Python
+job.run(function_name, cache_lock_uid = ['$param1', '-', '$param2'])
+
+## $param1 and $param2 will be swapped in with the cooresponding value.
+
+```
+
+
+
+
 ### Managing Jobs Manually
 For some reason, you may want to manage the job state yourself. (bypassing the run function above)
 
 ```
+## Job proccessing  (When you have unique jobs happening on the queue)
+job.release_lock(cache_uid) # Takes a string of the uid used in the laravel job
+
+
 ## Job failed
 job.fail(exception) # Takes a string of the exception message, and fails the job. (both in the db and the queue)
 
--- src/laravel_queue/__init__.py
+++ src/laravel_queue/__init__.py
@@ -26,16 +26,18 @@ class Job:
     existing_job: bool = False
     raw_record: dict = None
     queue: any = None
+    params: dict = None
     
     @classmethod
-    def from_psycopg2(cls, record, queue=None):
+    def from_psycopg2(cls, record, queue=None, args_map=None, ):
         '''
             Creates a Job object from a record returned by psycopg2
         '''
         raw_record = record
         payload = json.loads(record[1])
         command = cls.__php_serialized_to_dict(payload['data']['command'])
-        return cls(
+        params = cls.__get_params_from_command(payload['data']['commandName'], command)
+        job = cls(
             id=record[0],
             uuid=payload['uuid'],
             display_name=payload['displayName'],
@@ -49,19 +51,53 @@ class Job:
             command=command,
             existing_job=True,
             raw_record=raw_record,
-            queue=queue
+            queue=queue,
+            params=params
         )
+        return job
+    
     @classmethod
     def __php_serialized_to_dict(cls, command: str) -> dict:
         command = phpserialize.loads(command.encode('utf-8'), object_hook=phpserialize.phpobject)
         command_dict = command._asdict()
-        output = {
-            key.decode(): val.decode() if isinstance(val, bytes) else val
-            for key, val in command_dict.items()
-        }
-        return output
+        command_dict_formatted = {}
+        for key in command_dict:
+            val = command_dict[key]
+            key = key.replace(b'\x00', b'')
+            if isinstance(key, bytes):
+                key = key.decode()
+            if isinstance(val, bytes):
+                val = val.decode()
+            command_dict_formatted[key] = val
+
+        return command_dict_formatted
     
-    def run(self, function:any, *args, **kwargs) -> bool:
+    @classmethod
+    def __get_params_from_command(cls, command_name:str, command: dict) -> dict:
+        params = {}
+        for key in command:
+            if command_name in key:
+                param = key.replace(command_name, '')
+                params[param] = command[key]
+        return params
+
+    def __get_cache_uid_from_config(self, cache_lock_uid: list) -> str:
+        params = self.params
+        cache_uid = None  
+        if cache_lock_uid:
+            cache_uid = str(self.command_name.split('\\')[-1]) 
+            for var in cache_lock_uid:
+                if var.startswith('$'):
+                    value = params[var[1:]]
+                    if not value is None:
+                        cache_uid = cache_uid + str(value)
+                else:
+                    cache_uid = cache_uid + var
+        return cache_uid
+    
+    def run(self, 
+            function:any, 
+            cache_lock_uid = None) -> bool:
         '''
             Runs the job
             function: any, the function to run
@@ -70,17 +106,34 @@ class Job:
 
             Returns True if the job is successful, False if the job fails
             On fail, fails the job.
-        '''
+        '''  
+        self.release_lock(cache_lock_uid)
         try:
-            function(*args, **kwargs)
+            function(**self.params)
             self.complete()
             return True
         except Exception as e:
             self.fail(str(e))
             return False
             
-            
-    
+    def release_lock(self, cache_lock_uid: str | list = None):
+        '''
+            Releases the lock on the job
+
+        '''
+        if not cache_lock_uid:
+            return
+        if isinstance(cache_lock_uid, list):
+            cache_lock_uid = self.__get_cache_uid_from_config(cache_lock_uid)
+        
+        connection = self.queue.connection
+        cursor = connection.cursor()
+        delete = "delete from {} where key like '%{}'".format(self.queue.cache_table, cache_lock_uid)
+        cursor.execute(delete)
+        connection.commit()
+        cursor.close()   
+
+
     def fail(self, exception: str):
         '''
             Fails the job, moving the record to the failed_jobs table and completing the job
@@ -89,6 +142,7 @@ class Job:
         '''
         connection = self.queue.connection
         cursor = connection.cursor()
+        exception = exception.replace("'", "''")
         insert = "INSERT INTO {} (connection, queue, payload, exception, failed_at) VALUES ('{}', '{}', '{}', '{}', '{}')".format(
             self.queue.failed_jobs_table,
             'database', 
@@ -123,18 +177,20 @@ class Queue:
     def __init__(self, connection: any, 
                  queue: str = 'python', 
                  jobs_table: str = 'jobs.jobs', 
-                 failed_jobs_table: str = 'jobs.failed_jobs'):
+                 failed_jobs_table: str = 'jobs.failed_jobs',
+                 cache_table: str = 'jobs.cache_locks'):
         '''
         connection: str or sqlalchemy.engine.base.Engine
         queue: str, default 'python', the queue name in the database
         jobs_table: str, default 'jobs.jobs', the table name for the jobs
         failed_jobs_table: str, default 'jobs.failed_jobs', the table name for the failed jobs
-
+        cache_table: str, default 'jobs.cache_locks', the table name for the cache
         '''
         self.connection = self.__connect(connection)
         self.queue = queue
         self.jobs_table = jobs_table
         self.failed_jobs_table = failed_jobs_table
+        self.cache_table = cache_table
         self.jobs = []
 
     def read(self) -> list:
@@ -167,7 +223,7 @@ class Queue:
 
     def __connect(self, connection: any) -> any:
         if not isinstance(connection, str):
-            connection = connection.url
+            connection = connection.url.render_as_string(hide_password=False)
         return self.__connect_with_psycopg2(connection)
         
 
